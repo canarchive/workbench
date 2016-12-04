dd95db6601c5aa1ba458ae4a1d7ce02c42e2d6b4
wangcancompany <iamwangcan@163.com>
2016-12-04 23:26:30 +0800

    底层模型统计操作功能调整

--- common/models/BaseModel.php
+++ common/models/BaseModel.php
@@ -130,28 +130,28 @@ class BaseModel extends ActiveRecord
 
     public function statisticRecord($data, $type)
     {
-return ;
         $keywordTypes = ['visit', 'signup'];
         if (in_array($type, $keywordTypes)) {
-            $keyword = new \spread\models\statistic\Keyword();
+            $keyword = $this->_newModel('statisticKeyword');
+            //print_r($keyword);exit();
             $keyword->recordData($data, $type);
         }
 
         $reportTypes = ['visit', 'signup'];
         if (in_array($type, $reportTypes)) {
-            $report = new \spread\models\statistic\Report();
+            $report = $this->_newModel('statisticReport');
             $report->recordData($data, $type);
         }
 
         $reportServiceTypes = ['signup'];
         if (in_array($type, $reportServiceTypes)) {
-            $reportServiceService = new \spread\models\statistic\ReportService();
-            $reportServiceService->recordData($data, $type);
+            $reportService = $this->_newModel('statisticReportService');
+            $reportService->recordData($data, $type);
         }
 
         $dispatchTypes = ['dispatch'];
         if (in_array($type, $dispatchTypes)) {
-            $dispatch = new \spread\models\statistic\Dispatch();
+            $dispatch = $this->_newModel('statisticDispatch');
             $dispatch->recordData($data, $type);
         }
     }
