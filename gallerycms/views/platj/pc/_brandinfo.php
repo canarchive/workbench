eb29f462a42f1ec8b440024c2f1dd39526af57b6
homework <iamwangcan@163.com>
2017-01-26 22:16:18 +0800

    家装平台开发

---
+++ gallerycms/views/platj/pc/_brandinfo.php
@@ -0,0 +1,69 @@
+<div class="shop wrapper">
+    <div class="fl shop_l">
+        <a href="#2">
+            <img src="http://60.205.145.0/filesys/spider/pages/asset/platj/img/f3dc5-2512.jpg" width="113" height="87" alt="晨阳水漆加盟" /></a>
+    </div>
+    <div class="fl shop_r">
+        <h2>晨阳水漆网友点评</h2>
+        <div class="shop_rChild">
+            <ul class="ul_one clearfix">
+                <li class="clearfix">
+                    <span class="fl">合作模式：</span>
+                    <ul class="fl clearfix">
+                        <li>区域代理</li></ul>
+                </li>
+                <li class="clearfix">
+                    <span class="fl">适合人群：</span>
+                    <ul class="fl clearfix">
+                        <li>自主创业</li></ul>
+                </li>
+                <li class="clearfix">
+                    <span class="fl">招商区域：</span>
+                    <ul class="fl">
+                        <li>全国</li></ul>
+                </li>
+            </ul>
+            <ul class="ul_two clearfix">
+                <li class="clearfix">
+                    <span class="same">最低投资额:</span>
+                    <a class="a_one" href="#2">5万</a></li>
+                <li class="current4 clearfix">
+                    <span class="same">加盟热线：</span>
+                    <span id="shoowtel" class="span_four">0312-8593****
+                        <!--<p class="map2">4000-500-555<span style="float:none;">转</span></p>
+                        <p class="map2">4000-500-555</p>--></span>
+                    <a title="晨阳水漆加盟热线" class="a_five" href="javascript:;" onclick="check_is_login2(137829,1,2,33,'晨阳水漆');return false;">
+                        <span id="chakan">查看</span></a>
+                    <span id="my_telephone" style="display:none">0312-8593111</span></li>
+                <span id="zongbudianhua" style="display:none;color: #df0000;float:left;font-size: 20px;font-style: italic;font-weight: bold;margin-left: -70px;margin-top: 5px;">0312-8593111</span></ul>
+        </div>
+    </div>
+    <p>
+    </p>
+    <!---->
+    <div class="detail_btnChild">
+        <ul class="clearfix">
+            <li class="li1">
+                <a href="javascript:;" onclick="showSQ(2)">在线咨询</a></li>
+            <li class="li2">
+                <a href="javascript:;" hideFocus="true" title="关注项目" onclick="javascript:attention('137829','晨阳水漆','9');_gaq.push(['_trackEvent', '关注', '点击', '标项页关注点击']);return false;">关注
+                    <span id="attentionCountNum"></span></a>
+            </li>
+            <script>$(function() {
+                    getAttentionCountByAjax('137829');
+                });</script>
+            <li class="li3">
+                <a href="javascript:;" hideFocus="true" title="快速获得加盟资料" onclick="subEditUserInfo('137829','1','10');_gaq.push(['_trackEvent', '留言', '点击', '标项页索要资料点击']);return false;">索要资料</a></li>
+            <!--<li class="li4"><a href="#2">QQ咨询</a></li>-->
+            <li class="li4">
+                <a rel="nofollow" style="cursor:pointer" onclick="qqdynamic(137829,4006122505)" id="clickqq">QQ咨询</a></li>
+            <li class="li5">
+                <span>
+                    <a href="javascript:;" hideFocus="true" title="想知道更多项目详情？让企业给您回电话吧!" onclick="subEditUserInfo2('137829','3','12',1,2);_gaq.push(['_trackEvent', '留言', '点击', '标项页回电话点击']);return false;">企业回拨</a></span>
+            </li>
+            <div class="clr"></div>
+        </ul>
+        <div class=yj2></div>
+        <div class=yj1></div>
+    </div>
+</div>
