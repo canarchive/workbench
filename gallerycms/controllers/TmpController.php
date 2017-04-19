a65c81a8c4f9132a937f1014f1918bd25190db1b
wangcancompany <iamwangcan@163.com>
2017-04-19 11:22:13 +0800

    生成统计sql

--- gallerycms/controllers/TmpController.php
+++ gallerycms/controllers/TmpController.php
@@ -15,6 +15,59 @@ class TmpController extends GallerycmsController
         $this->$action();
     }   
 
+    public function service()
+    {
+        $sqlInsert = 'TRUNCATE `workad_statistic_decoration`.`ws_service_origin`;INSERT INTO `workad_statistic_decoration`.`ws_service_origin` (
+                `city_code`, `merchant_id`, `client_type`, `service_id`, `channel`, `created_month`, `created_week`, `created_weekday`, `created_day`)
+                SELECT `city_code`, `merchant_id`, `client_type`, `service_id`, `channel`, `created_month`, `created_week`, `created_weekday`, `created_day` FROM `workad_decoration`.`wd_user` GROUP BY `city_code`, `merchant_id`, `client_type`, `service_id`, `channel`, `created_month`, `created_week`, `created_weekday`, `created_day`;<br />';
+        $sqlBase = "UPDATE `workad_statistic_decoration`.`ws_service_origin` AS `a`, 
+            (SELECT `city_code`, `merchant_id`, `client_type`, `service_id`, `channel`, `created_month`, `created_week`, `created_weekday`, `created_day`, COUNT(*) AS `count` FROM `workad_decoration`.`wd_user` WHERE {{WHERE}} GROUP BY `city_code`, `merchant_id`, `client_type`, `service_id`, `channel`, `created_month`, `created_week`, `created_weekday`, `created_day`) AS `b` 
+            SET `a`.`{{UPFIELD}}` = `b`.`count` 
+            WHERE `a`.`city_code` = `b`.`city_code` AND `a`.`merchant_id` = `b`.`merchant_id` AND `a`.`client_type` = `b`.`client_type` AND `a`.`service_id` = `b`.`service_id` AND `a`.`channel` = `b`.`channel` AND `a`.`created_month` = `b`.`created_month` AND `a`.`created_week` = `b`.`created_week` AND `a`.`created_weekday` = `b`.`created_weekday` AND `a`.`created_day` = `b`.`created_day`;";
+
+        $elems = [
+            'all' => 'visit_num_success',
+            '' => 'new_num',
+            'follow' => 'follow_num',
+            'follow-plan' => 'followplan_num',
+            'valid' => 'valid_num',
+            'valid-out' => 'validout_num',
+            'valid-dispatch' => 'validdispatch_num',
+            'bad' => 'bad_num',
+        ];
+
+        $badElems = [
+            '' => 'badnew_num',
+            'no_call' => 'badnocall_num',
+            'noneed' => 'badnoneed_num',
+            'booked' => 'badbooked_num',
+            'no_test' => 'badtest_num',
+        ];
+
+        $outStr = $sqlInsert;
+        foreach ($elems as $elem => $elemField) {
+            $where = $elem == 'all' ? "`service_num` = 0" : "`service_num` = 0 AND `status` = '{$elem}'";
+            $sql = str_replace(['{{WHERE}}', '{{UPFIELD}}'], [$where, $elemField], $sqlBase) . '<br />';
+            $outStr .= $sql;
+            $where = $elem == 'all' ? "`service_num` > 0" : "`service_num` > 0 AND `status` = '{$elem}'";
+            $elemFieldOld = 'old_' . $elemField;
+            $sql = str_replace(['{{WHERE}}', '{{UPFIELD}}'], [$where, $elemFieldOld], $sqlBase) . '<br />';
+            $outStr .= $sql;
+        }
+
+        foreach ($badElems as $elem => $elemField) {
+            $where = "`service_num` = 0 AND `status` = 'bad' AND `invalid_status` = '{$elem}'";
+            $sql = str_replace(['{{WHERE}}', '{{UPFIELD}}'], [$where, $elemField], $sqlBase) . '<br />';
+            $outStr .= $sql;
+            $where = "`service_num` > 0 AND `status` = 'bad' AND `invalid_status` = '{$elem}'";
+            $elemFieldOld = 'old_' . $elemField;
+            $sql = str_replace(['{{WHERE}}', '{{UPFIELD}}'], [$where, $elemFieldOld], $sqlBase) . '<br />';
+            $outStr .= $sql;
+        }
+
+        echo $outStr;
+    }
+
     protected function updateCmsad()
     {   
         $model = new Article();
