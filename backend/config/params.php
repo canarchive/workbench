58957cbc96d61654d1847df8a7b57ea7546c4cc6
wangcancompany <iamwangcan@163.com>
2017-05-18 11:03:58 +0800

    plat开发

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,3 +1,21 @@
 <?php
 return [
+    'moduleInfos' => [
+        '' => '后台基本模块',
+        'passport' => '用户模块',
+        'website' => '店铺模块',
+        'paytrade' => '充值交易模块',
+        'saccount' => '推广账户',
+        'spread' => '推广',
+        'subsite' => '平台',
+        'subsite/decoration' => '家装推广',
+        'subsite/kl' => '第三方推广',
+        'merchant' => '商家',
+        'statistic' => '推广统计',
+
+        'spread/decoration' => '',
+        'statistic/decoration' => '',
+        'spread/shoot' => '',
+        'spread/third' => '',
+    ],
 ];
