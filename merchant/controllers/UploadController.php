c68c079de34436f6917f90dfa153ab130b3e6c45
homejob <iamwangcan@163.com>
2017-06-09 08:03:26 +0800

    merchant 开发

--- merchant/controllers/UploadController.php
+++ merchant/controllers/UploadController.php
@@ -4,11 +4,8 @@ namespace merchant\controllers;
 
 use common\components\UploadController;
 use merchant\models\Attachment;
-use yii\filters\Cors;
-use yii\helpers\ArrayHelper;
-use yii\web\UploadedFile;
 
-class MerchantUploadController extends UploadController
+class UploadController extends UploadController
 {
     public $enableCsrfValidation = false;
 
