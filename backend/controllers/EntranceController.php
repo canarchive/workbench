06cd6311b2ba8cbbc10899b6d4b26051568d7c1f
homejob <iamwangcan@163.com>
2017-06-09 08:03:57 +0800

    底层功能调整

---
+++ backend/controllers/EntranceController.php
@@ -0,0 +1,21 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use backend\models\Signin;
+use backend\components\AdminController;
+use common\auth\controllers\EntranceTrait;
+
+class EntranceController extends AdminController
+{
+	use EntranceTrait;
+
+	public $layout = false;
+
+
+	public function getModel($sort)
+	{
+		return new Signin();
+	}
+}
