2dcd868c398330676bf9eb36f97a64abf65f562d
wangcancompany <iamwangcan@163.com>
2017-07-02 19:41:02 +0800

    develop plat

--- subsite/controllers/SiteController.php
+++ subsite/controllers/SiteController.php
@@ -44,19 +44,13 @@ class SiteController extends SubsiteController
     public function actionHdenter()
     {
         $sourceCode = Yii::$app->request->get('source_code', '');
-		$domain = isset($this->domainDatas[$sourceCode]) ? $this->domainDatas[$sourceCode] : false;
-		if (empty($domain)) {
-		    $url = Yii::getAlias('@tbz.subsiteurl') . '/baom-quote-beijing.html';
-            header("Location:$url");
-            exit();
-		}
-
-        if (isset($_GET['source_code'])) {
-            unset($_GET['source_code']);
+        $clientType = $this->clientType;
+        if (!$this->isMobile) {
+            $url = Yii::getAlias('@default.subsiteurl') . '/bm-designz-beijing.html';
+        } else {
+            $url = Yii::getAlias('@m.default.subsiteurl') . '/bm-tquote-beijing.html';
         }
-        $queryStr = http_build_query($_GET);
-        $url = $domain->mobileDomain . '/' . $sourceCode . '.html?' . $queryStr;
-        
+
         header("Location:$url");
     }
 }
