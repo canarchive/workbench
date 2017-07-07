918b4f360930a49e7ae3a3dc6da4dc9502ea0042
homejob <iamwangcan@163.com>
2017-07-07 14:16:53 +0800

    develop backend

--- baseapp/shop/views/goods/_form.php
+++ baseapp/shop/views/goods/_form.php
@@ -13,6 +13,8 @@ use shop\models\Category;
     <?= $form->field($model, 'price')->textInput() ?>
     <?= $form->field($model, 'price_market')->textInput() ?>
     <?= $form->field($model, 'orderlist')->textInput() ?>
+    <?= $form->field($model, 'slide')->hiddenInput(); ?>
+	<?= $model->uploadElem('goods', 'slide'); ?>
     <?= $form->field($model, 'picture')->hiddenInput(); ?>
 	<?= $model->uploadElem('goods', 'picture'); ?>
     <?= $form->field($model, 'keyword')->textInput(['maxlength' => 128]) ?>
