afdbcc31214dc32ff5f4e2ba06b11c6b39fcbff0
wangcancompany <iamwangcan@163.com>
2017-07-15 17:40:12 +0800

    develop backend

--- common/components/Controller.php
+++ common/components/Controller.php
@@ -51,7 +51,19 @@ class Controller extends YiiController
         parent::init();
 
         $this->host = Yii::$app->request->hostInfo;
-        $this->clientUrl = Yii::$app->request->url;
+        $this->clientUrl = $url = Yii::$app->request->url;
+        if (strpos($url, '.html') === false && strpos($url, '.xml') === false) {
+            $pos = strpos($this->clientUrl, '?');
+            $query = $pos !== false ? substr($url, $pos) : '';
+            $urlBase = str_replace($query, '', $url);
+            $lastChar = substr($url, -1);
+            if ($lastChar != '/') {
+                $rUrl = "{$this->host}{$urlBase}/{$query}";
+                //header("Location: {$url}");
+                return Yii::$app->response->redirect($rUrl, 301)->send();
+                exit();
+            }
+        }
         if (strpos($this->clientUrl, 'index.php') !== false) {
             return Yii::$app->response->redirect($this->host)->send();
             exit();
