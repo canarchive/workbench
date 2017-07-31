873841464952d8e54d759c7c212f58ce6cac28f6
wangcancompany <iamwangcan@163.com>
2017-07-31 10:15:47 +0800

    develop plat

--- subsite/decoration/models/Callback.php
+++ subsite/decoration/models/Callback.php
@@ -8,4 +8,26 @@ use baseapp\spread\models\CallbackTrait;
 class Callback extends ModelBase
 {
     use CallbackTrait;
+
+    public function getStatusInfos()
+    {
+        return $this->_userStatus('status');
+    }
+
+    public function getInvalidStatusInfos()
+    {
+        return $this->_userStatus('invalidStatus');
+    }
+
+    public function getOutStatusInfos()
+    {
+        return $this->_userStatus('outStatus');
+    }
+
+    protected function _userStatus($type)
+    {
+        $uModel = New User();
+        $field = $type . 'Infos';
+        return $uModel->$field;
+    }
 }
