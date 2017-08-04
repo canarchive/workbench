6c525d65e88c92832f504911db67552cf625b89e
wangcancompany <iamwangcan@163.com>
2017-08-04 10:37:26 +0800

    develop plat

--- merchant/decoration/views/layouts/main-base.php
+++ merchant/decoration/views/layouts/main-base.php
@@ -1,8 +1,6 @@
 <?php
 use yii\helpers\Url;
 
-$this->params['faviconUrl'] = Yii::getAlias('@asseturl') . '/third/eale/images/ficon.png';
-
 $cssFiles = ['website'];;
 $this->params['cssStr'] = $this->render('@common/views/base/_css-js', ['files' => $cssFiles, 'path' => 'merchant/adview/css']);
 $jsFiles = ['modernizr'];;
