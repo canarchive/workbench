6e4beead788faf87c6be9e4507d57a993c19b9db
wangcancompany <iamwangcan@163.com>
2017-08-05 11:53:10 +0800

    develop backend

--- baseapp/spread/models/searchs/UserMerchantTrait.php
+++ baseapp/spread/models/searchs/UserMerchantTrait.php
@@ -6,9 +6,6 @@ use yii\data\ActiveDataProvider;
 
 Trait UserMerchantTrait
 {
-    public $created_at_start;
-    public $created_at_end;
-
     public function rules()
     {
         return [
@@ -16,34 +13,15 @@ Trait UserMerchantTrait
         ];
     }
 
-    public function search($params)
+    protected function _searchElems()
     {
-        $query = self::find()->orderBy('id DESC');
-
-        $dataProvider = new ActiveDataProvider(['query' => $query]);
-        if ($this->load($params, '') && !$this->validate()) {
-            return $dataProvider;
-        }
-        if (!empty($this->mobile)) {
-            $query->andFilterWhere(['like', 'mobile', $this->mobile]);
-        }
-
-        $query->andFilterWhere([
-            'merchant_id' => $this->merchant_id,
-            'service_id' => $this->service_id,
-        ]);
-        $this->status = $this->status == 'all' ? null : $this->status;
-        $this->status = $this->status == 'back' ? ['back_reply', 'back_confirm'] : $this->status;
-		if ($this->status !== null) {
-			$query->andWhere(['status' => $this->status]);
-		}
-        //$this->merchant_id = 0;
-
-        $startTime = intval(strtotime($this->created_at_start));
-        $endTime = $this->created_at_end > 0 ? intval(strtotime($this->created_at_end)) : time();
-        $query->andFilterWhere(['>=', 'created_at', $startTime]);
-        $query->andFilterWhere(['<', 'created_at', $endTime]);
-        return $dataProvider;
+        return [
+            ['field' => 'mobile', 'type' => 'common', 'sort' => 'like'],
+            ['field' => 'service_id', 'type' => 'common'],
+            ['field' => 'merchant_id', 'type' => 'common'],
+            ['field' => 'status', 'type' => 'common'],
+            ['field' => 'created_at', 'type' => 'rangeTime'],
+        ];
     }
 
     public function _searchDatas()
