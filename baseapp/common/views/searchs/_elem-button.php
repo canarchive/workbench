3f0d84f239894d25b73fa9da24caac018e276545
wangcancompany <iamwangcan@163.com>
2017-08-11 23:07:01 +0800

    develop backend

--- baseapp/common/views/searchs/_elem-button.php
+++ baseapp/common/views/searchs/_elem-button.php
@@ -1,10 +1,10 @@
 <?php
 use yii\helpers\Html;
-$name = isset($elem['name']) ? $elem['name'] : '搜索';
-$buttons = isset($elem['buttons']) ? $elem['button'] : [['name' => '搜索', 'option' => ['class' => 'btn btn-primary']]];
+$optionDefault = ['class' => 'btn btn-primary'];
+$buttons = isset($elem['buttons']) ? $elem['buttons'] : [['name' => '搜索']];
 ?>
 <?php if (isset($haveWrap)) { echo '<div class="form-group form-group-sm">'; } ?>
-    <div class="col-md-2">
-        <?php foreach ($buttons as $button) { echo Html::submitButton($button['name'], $button['option']); } ?>
+    <div class="col-md-3">
+        <?php foreach ($buttons as $button) { $option = isset($button['option']) ? array_merge($optionDefault, $button['option']) : $optionDefault; echo Html::submitButton($button['name'], $option) . '    '; } ?>
     </div>
 <?php if (isset($haveWrap)) { echo '</div>'; } ?>
