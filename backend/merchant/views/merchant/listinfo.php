572b2901ed45b60507aec34463d9bbe27e5bccae
wangcancompany <iamwangcan@163.com>
2017-08-17 19:48:12 +0800

    develop merchant

--- backend/merchant/views/merchant/listinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$gridViewParams = [
-    'dataProvider' => $dataProvider,
-    'columns' => $this->context->searchModel->formatTemplateDatas('list', $this),
-];
-
-echo $this->render('@backend/views/common/listinfo', ['gridViewParams'  => $gridViewParams]);
