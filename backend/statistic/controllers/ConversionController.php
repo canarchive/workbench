fbf086832540e67fb71638f250f41ac52149a424
wangcancompany <iamwangcan@163.com>
2017-08-19 17:42:44 +0800

    develop merchant

--- backend/statistic/controllers/ConversionController.php
+++ backend/statistic/controllers/ConversionController.php
@@ -7,8 +7,12 @@ use backend\components\ControllerTraitView;
 
 class ConversionController extends AdminController
 {
-    public $viewPrefix = '@backend/statistic/views/conversion/';
     use ControllerTraitView;
     protected $modelClass = 'baseapp\statistic\models\Conversion';
     protected $modelSearchClass = 'baseapp\statistic\models\searchs\Conversion';
+
+    public function getListinfoView()
+    {
+        return '@backend/statistic/views/conversion/listinfo';
+    }
 }
