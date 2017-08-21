dee5adf9c8ddf6badca021bab689df05c740ee1d
wangcancompany <iamwangcan@163.com>
2017-08-21 16:09:40 +0800

    develop plat

--- baseapp/config/params-menu-modules.php
+++ baseapp/config/params-menu-modules.php
@@ -2,13 +2,17 @@
 return [
 	'backend' => [
         '' => '后台基本模块',
-        'statistic' => '推广统计',
-        'spread' => '推广',
         'merchant' => '商家',
+        'spread' => '推广',
+        'statistic' => '推广统计',
+        'subsite' => '平台',
+        'subsite/decoration' => '家装推广',
+        'subsite/kl' => '第三方推广',
     ],
 	'merchant' => [
 		'admin' => '基本模块',
 		'admin/statistic' => '统计',
 		'admin/merchant' => '商家',
+        'admin/decoration' => '家装推广',
 	],
 ];
