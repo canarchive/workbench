240375fa5352085434fdf65d516bb82532068df8
wangcancompany <iamwangcan@163.com>
2017-08-23 16:54:59 +0800

    develop merchant

--- backend/spread/views/plan/_form.php
+++ backend/spread/views/plan/_form.php
@@ -21,7 +21,7 @@ use common\widgets\FileUploadUI;
             $('#plan-end_at').datetimepicker({locale: 'zh-CN', format: 'YYYY-MM-DD HH:mm:ss'});
         });
     </script>
-    <?= $form->field($model, 'merchant_id')->dropDownList($model->getPointInfos('merchant', ['where' => ['status' => 2]]), ['prompt' => '']); ?>
+    <?= $form->field($model, 'merchant_id')->dropDownList($model->getPointInfos('merchant', ['where' => ['status' => 'cpa']]), ['prompt' => '']); ?>
     <?= $form->field($model, 'status')->dropDownList($model->statusInfos, ['prompt' => '']); ?>
     <?= $this->render('@backend/views/common/form_button', ['model' => $model]); ?>
     <?php ActiveForm::end(); ?>
