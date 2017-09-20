a7dfac66318cbe897745db2cbf7cd5b971297334
wangcancompany <iamwangcan@163.com>
2017-09-20 13:57:00 +0800

    develop backend

--- baseapp/ifeed/views/adpicture/_form.php
+++ baseapp/ifeed/views/adpicture/_form.php
@@ -14,16 +14,20 @@ $breadCrumbExt = !empty($model->site_code) ? $model->siteInfos[$model->site_code
 $breadCrumbExt .= !empty($model->page) ? '-' . $pageInfos[$model->page] : '';
 $breadCrumbExt .= !empty($model->position) ? '-' . $model->positionInfos[$model->position] : '';
 $this->params['breadCrumbExt'] = $breadCrumbExt;
+$model->thumb = $model->getAttachmentIds('adpicture', 'thumb');
 ?>
 
 <div class="menu-form">
-
     <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+    <input type="hidden" name="site_code" value="<?= $model->site_code; ?>" />
+    <input type="hidden" name="position" value="<?= $model->position; ?>" />
+    <input type="hidden" name="page" value="<?= $model->page; ?>" />
     <?= $form->field($model, 'name')->textInput() ?>
     <?= $form->field($model, 'orderlist')->textInput() ?>
     <?= $form->field($model, 'url')->textInput() ?>
     <?php //echo $form->field($model, 'thumb')->hiddenInput()->label($label); ?>
-	<?= $form->field($model, 'thumb')->hiddenInput(); ?>
+    <?= $form->field($model, 'thumb')->hiddenInput(); ?>
+    <?= $model->uploadElem('adpicture', 'thumb'); ?>
     <?= $form->field($model, 'status')->dropDownList($model->statusInfos, ['prompt' => '']); ?>
 
 	<?= $this->render('@app/views/common/form_button', ['model' => $model]); ?>
