fc2247509e3eb0d6767a19da0cd61c5057ef65fc
homejob <iamwangcan@163.com>
2017-09-25 16:53:31 +0800

    develop backend

--- common/smser/config/params-verification.php
+++ common/smser/config/params-verification.php
@@ -1,12 +1,12 @@
 <?php
 $configInfo = [
-    'siginup' => [
+    'signup' => [
         'length' => 4, // 验证码长度，可以设置动态长度，如 [4,8]
         'sleep' => 60, // 两次发送时间间隔
         'sendTimes' => 5, // 同一个手机号每天发送频率
         'expire' => 300, // 验证码有效时间
     ],
-    'signin-auto' => [
+    'signupin' => [
         'length' => 4,
         'sleep' => 1,
         'sendTimes' => 100,
