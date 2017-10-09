01712801b1c2c2809f157cebc154feb1af23c778
wangcancompany <iamwangcan@163.com>
2017-10-09 22:25:58 +0800

    develop ifeed shoot

--- ifeed/shoot/controllers/AboutusController.php
+++ ifeed/shoot/controllers/AboutusController.php
@@ -44,4 +44,42 @@ class AboutusController extends Controller
         $return = $model->doSubmitInfo();
         return $return;
     }
+
+    public function getContactDatas()
+    {
+        $datas = [
+            'hstudio' => [
+                'hotline' => '15201631163',
+                'serviceTime' => '周一至周日 9：00 ~ 18：00',
+                'address' => '北京市朝阳区百子湾路苹果社区北区二十二院艺术街6号楼地下一层B3-5',
+                //'weibo' => 'http://weibo.com/turuiyingxiang',
+                'wechat' => 'jingweifengshang',
+                //'line' => '地铁7号线百子湾站A口出向东150米',
+                'position' => '116.477496,39.905321',
+                'width' => '550',
+                'height' => '60',
+            ],
+            'shoot' => [
+                'hotline' => Yii::$app->params['siteHotline'],
+                'serviceTime' => '周一至周日 9：00 ~ 18：00',
+                'address' => '北京市朝阳区广渠路3号竞园产业基地10A',
+                'weibo' => 'http://weibo.com/turuiyingxiang',
+                'wechat' => 'hlyxwx',
+                'line' => '乘车路线：地铁7号线百子湾站A口出向东150米',
+                'position' => '116.516407,39.901525',
+                'width' => '400',
+                'height' => '60',
+            ],
+            'eale' => [
+                'hotline' => '400-090-1916',
+                'serviceTime' => '周一至周六 9:30-19:00',
+		        'address' => '北京市朝阳区高碑店文化新大街东辰民族艺术大厦D座-03',
+                'line' => '乘坐541，169小郊亭下车。坐八通线高碑店下，步行十五分钟到。',
+                'position' => '116.547801,39.904919',
+                'width' => '450',
+                'height' => '50',
+            ],
+        ];
+        return $datas[$this->siteCode];
+    }
 }
--- ifeed/shoot/views/hulian/pc/aboutus/contactus.php
+++ ifeed/shoot/views/hulian/pc/aboutus/contactus.php
@@ -1,1 +1,39 @@
-<?= $this->render('_contactus-' . $this->context->siteCode); ?>
+<?php
+$data = $this->context->getContactDatas(); 
+?>
+<script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=KzlZ4l6eMZHvnakAW6MjTc5dOhFKT4VX"></script>
+<div class="contact c">
+    <div class="contact_container fl">
+        <h2><?= $this->context->currentSiteInfo['name']; ?></h2>
+        <ul>
+            <li>咨询电话：<?= $data['hotline']; ?></li>
+            <li>营业时间：<?= $data['serviceTime']; ?></li>
+            <?php if (!empty($data['weibo'])) { ?><li>微博：<?= $data['weibo']; ?></li><?php } ?>
+            <?php if (!empty($data['wechat'])) { ?><li>微信：<?= $data['wechat']; ?></li><?php } ?>
+		    <li>地址：<?= $data['address']; ?></li>
+        </ul>
+        <?php if (!empty($data['line'])) { ?><p>乘车路线：<?= $data['line']; ?></p><?php } ?>
+        <a class="server_btns" href="/apply.html" rel="nofollow" title="预约拍摄">预约到店</a>
+    </div>
+    <div id="allmap" class="contact_map fl"></div>
+</div>
+<script>
+
+    var map = new BMap.Map("allmap");
+    map.addControl(new BMap.MapTypeControl());
+    map.setCurrentCity("北京");
+    var point = new BMap.Point(<?= $data['position']; ?>);
+    var marker = new BMap.Marker(point);
+    map.addOverlay(marker);
+    map.centerAndZoom(point, 16);
+    var opts = {
+        width : <?= $data['width']; ?>,     // 信息窗口宽度
+        height: <?= $data['height']; ?>,     // 信息窗口高度
+        title : "<?= $this->context->currentSiteInfo['name']; ?>" ,// 信息窗口标题
+        position : point,    // 指定文本标注所在的地理位置
+        offset   : new BMap.Size(10,-10)    //设置文本偏移量
+    }
+    var infoWindow = new BMap.InfoWindow("<?= $data['address']; ?>", opts);  // 创建信息窗口对象
+    map.openInfoWindow(infoWindow,point);
+
+</script>
