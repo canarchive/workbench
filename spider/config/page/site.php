48f470ab23f346a9676a72c0ec6daed7b1106bbc
wangcancompany <iamwangcan@163.com>
2017-11-01 16:06:03 +0800

    develop spider

--- spider/config/page/site.php
+++ spider/config/page/site.php
@@ -1,11 +1,15 @@
 <?php
 return [
-    'salesys' => [
+    /*'salesys' => [
         'code' => 'merchant',
         'name' => '销售系统',
     ],
     'flower' => [
         'code' => 'huasl',
         'name' => '花加',
+    ],*/
+    'spread' => [
+        'code' => 'spreadnew',
+        'name' => '推广页',
     ],
 ];
